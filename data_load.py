cd7707bd7c8458b5c6c8693f8c65ef1c9ef8ae00
wenwenlee <leejamesgm@gmail.com>
2018-05-08 14:38:09 +0800

    Update data_load.py

--- data_load.py
+++ data_load.py
@@ -3,20 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from PIL import Image
 
-filePath = "D:\\code\\NotePad\\SCUT-FBP5500_v2\\Images\\"
-
-
-def load_data(fileName):
+def load_data(textPath,ImgPath):
 	x_img = []
 	y_label = []
 	
-	file = open(fileName,"r") 
+	file = open(textPath,"r") 
 	data = file.readlines()
 	for line in data:
 		index = line.index('g')  
 		imgName = line[0:index+1]
 		label = line[index+2:index+7]
-		img = filePath+imgName
+		img = ImgPath+imgName
 		im = np.array(Image.open(img))
 		x_img.append(im) 
 		label_np = np.array(label)
@@ -26,6 +23,8 @@ def load_data(fileName):
 
 if __name__ == "__main__":    
 
-	file = "D:\\code\\NotePad\\SCUT-FBP5500_v2\\train_test_files\\split_of_60%training and 40%testing\\train.txt"
-	x_train,y_train = load_data(file)
-	
+	textPath = "D:\\code\\NotePad\\SCUT-FBP5500_v2\\train_test_files\\split_of_60%training and 40%testing\\train.txt"
+	textPath_test = "D:\\code\\NotePad\\SCUT-FBP5500_v2\\train_test_files\\split_of_60%training and 40%testing\\test.txt"
+	ImgPath = "D:\\code\\NotePad\\SCUT-FBP5500_v2\\Images\\"
+	x_train,y_train = load_data(textPath,ImgPath)
+	x_test,y_test = load_data(textPath_test,ImgPath)
