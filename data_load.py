3d4c752e9c91fe6f708968b2dcaf7b3fd5b65cfa
wenwenlee <leejamesgm@gmail.com>
2018-05-08 10:55:23 +0800

    Update data_load.py

--- data_load.py
+++ data_load.py
@@ -2,18 +2,23 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt 
 from PIL import Image
-def load_label(fileName):
+
+filePath = "D:\\code\\NotePad\\SCUT-FBP5500_v2\\Images\\"
+
+def load_data(fileName):
+	x_img = []
+	y_label = []
 	
 	file = open(fileName,"r") 
 	data = file.readlines()
 	for line in data:
 		index = line.index('g')  
-		print(line[0:index+1])
-		print(line[index+2:index+7])
+		imgName = line[0:index+1]
+		label = line[index+2:index+7]
+		img = filePath+imgName
+		
 		words = line
-	
 
-	filePath = "D:\\code\\NotePad\\SCUT-FBP5500_v2\\Images\\"
 	fileName = words[0:9]
 	label = words[10:16]
 	print(filePath+fileName)
@@ -22,13 +27,15 @@ def load_label(fileName):
 	img = filePath+fileName
 	print(img)
 	im = np.array(Image.open(img))
+	# tmp = Image.open(img)
 	plt.imshow(im)
 	plt.axis('off')
+	plt.show()
 	
 if __name__ == "__main__":    
 
 	file = "D:\\code\\NotePad\\SCUT-FBP5500_v2\\train_test_files\\split_of_60%training and 40%testing\\train.txt"
-	load_label(file)
+	load_data(file)
 	sStr1 = 'strchr'   
 	sStr = 's'   
 	nPos = sStr1.index(sStr)   
