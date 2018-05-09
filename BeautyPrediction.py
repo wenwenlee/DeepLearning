721c793ff446a276a0a77e0ae7bc6a8e7cf40f95
wenwenlee <leejamesgm@gmail.com>
2018-05-09 14:14:39 +0800

    Update BeautyPrediction.py

--- BeautyPrediction.py
+++ BeautyPrediction.py
@@ -9,9 +9,12 @@ from keras.layers import Dense, Activation,GlobalAveragePooling2D
 from keras.applications.resnet50 import ResNet50
 from keras import optimizers
 
+import h5py
 ImgPath2 = "D:\\code\\NotePad\\SCUT-FBP5500_v2\\Images\\"
 img_rows = 350
 img_cols = 350
+
+NB_ResNet_LAYERS_TO_FREEZE = 49
 def process_line(line,ImgPath):
 
 	index = line.index('g')  
@@ -59,27 +62,44 @@ def load_data(textPath,ImgPath):
 	# print(im)	
 	return x_img,y_label
 
+def setup_to_finetune(model):
+  """Freeze the bottom NB_IV3_LAYERS and retrain the remaining top layers.
+  note: NB_IV3_LAYERS corresponds to the top 2 inception blocks in the inceptionv3 arch
+  Args:
+    model: keras model
+  """
+  for layer in model.layers[:NB_ResNet_LAYERS_TO_FREEZE]:
+     layer.trainable = False
+  for layer in model.layers[NB_ResNet_LAYERS_TO_FREEZE:]:
+     layer.trainable = True
+  # model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])	
+
+def train(textPath,ImgPath):
+	basemodel = ResNet50(include_top=False, pooling='avg')
+	a = basemodel.output
+	#a = GlobalAveragePooling4D()(a)
+	b = Dense(1)(a)
+	model = Model(inputs=basemodel.input, outputs=b)
+	#setup_to_finetune(model)
+	model.layers[0].trainable = False
+	print(model.summary())
+
+	adam = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+	model.compile(loss='mean_squared_error', optimizer=adam)
+	
+	history_ft = model.fit_generator(generate_arrays_from_file(textPath,ImgPath),
+                    steps_per_epoch=1000, epochs=10)
+	# model.save("D:\\code\\NotePad\\ResNet50.model")
+		#保存神经网络的结构与训练好的参数
+	json_string = model.to_json()#等价于 json_string = model.get_config()  
+	open('D:\\code\\NotePad\\my_model_architecture.json','w').write(json_string)    
+	model.save_weights('D:\\code\\NotePad\\my_model_weights.h5')
 
 if __name__ == "__main__":    
 
 	textPath = "D:\\code\\NotePad\\SCUT-FBP5500_v2\\train_test_files\\split_of_60%training and 40%testing\\train.txt"
 	textPath_test = "D:\\code\\NotePad\\SCUT-FBP5500_v2\\train_test_files\\split_of_60%training and 40%testing\\test.txt"
 	ImgPath = "D:\\code\\NotePad\\SCUT-FBP5500_v2\\Images\\"
-	# x_train,y_train = load_data(textPath,ImgPath)
-	# x_test,y_test = load_data(textPath,ImgPath)
-	
-	basemodel = ResNet50(include_top=False, pooling='avg')
-	a = basemodel.output
-	#a = GlobalAveragePooling4D()(a)
-	b = Dense(1)(a)
-	model = Model(inputs=basemodel.input, outputs=b)
-	model.layers[0].trainable = False
-	print(model.summary())
 
-	# adam = ks.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0) 
-	adam = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-	model.compile(loss='mean_squared_error', optimizer=adam)
-	# model.fit(batch_size=32, x=train_X, y=train_Y, epochs=30)
-	history_ft = model.fit_generator(generate_arrays_from_file(textPath,ImgPath),
-                    steps_per_epoch=1000, epochs=10)
-	model.save("D:\\code\\NotePad\\ResNet50.model")
+	train(textPath,ImgPath)
+	
